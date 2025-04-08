6f79c86a75d8105d86be4efb240cfd9ab28259cd
malwinaniedziolka <mmalwina.niedziolka@gmail.com>
2025-04-08 18:41:29 +0200

    update 2

--- constants/navigation.js
+++ constants/navigation.js
@@ -1,10 +1,32 @@
-const HOME_LINK = {};
-// Stwórz stałą HOME_LINK.
-// Podpowiedź: const HOME_LINK = { ... };
+const HOME_LINK = {
+    label: "Home",
+    path: "/"
+};
 
-const LOGOUT_LINKS = {};
-// Stwórz stałą LOGOUT_LINKS.
-// Podpowiedź: const LOGOUT_LINKS = [ ... ];
+const LOGOUT_LINKS = [
+    HOME_LINK,
+    {
+      label: "Logout from application",
+      path: "/kill"
+    }
+];
 
-const MENU_LINKS = {};
-// Stwórz stałą MENU_LINKS.
+const MENU_LINKS = [
+    HOME_LINK,
+    {
+      label: "Products",
+      path: "/products"
+    },
+    {
+        label: "add product",
+        path: "/products/add"
+    },
+    {
+        label: "Newest product",
+        path: "/products/new"
+    },
+    {
+        label: "Logout",
+        path: "/logout"
+    }
+];
